ce9ac7e4494981368a238c58dba2c1ca15b0b456
Ilie Sochirca <Ilie.Sochirca@orange.com>
2018-12-06 16:55:07 +0200

    Changes added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a description',
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.' +
+      'myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%' +
+      '2Frecipes%2Fsl%2F13%2F07%2Ffirecracker-grilled-salmon-sl-x.jpg%3Fitok%3DDXNiVdiS&w=1000&c=sc&poi=face&q=70')
+
+];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
