220c717af19c253f8f29cf44dd479c0e16905ffb
Ilie Sochirca <Ilie.Sochirca@orange.com>
2018-12-07 15:42:34 +0200

    Adding Shoppinglist and recipes HTML

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,9 +13,17 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply a description',
       'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.' +
       'myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%' +
+      '2Frecipes%2Fsl%2F13%2F07%2Ffirecracker-grilled-salmon-sl-x.jpg%3Fitok%3DDXNiVdiS&w=1000&c=sc&poi=face&q=70'),
+    new Recipe('A Test Recipe', 'This is simply a description',
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.' +
+      'myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%' +
+      '2Frecipes%2Fsl%2F13%2F07%2Ffirecracker-grilled-salmon-sl-x.jpg%3Fitok%3DDXNiVdiS&w=1000&c=sc&poi=face&q=70'),
+    new Recipe('A Test Recipe', 'This is simply a description',
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.' +
+      'myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%' +
       '2Frecipes%2Fsl%2F13%2F07%2Ffirecracker-grilled-salmon-sl-x.jpg%3Fitok%3DDXNiVdiS&w=1000&c=sc&poi=face&q=70')
 
-];
+  ];
 
   constructor() {
   }
